621da11e700b5ae91d238c2b053c800959943a69
yoppy0x100 <anggaraputra456@gmail.com>
2024-09-28 17:35:33 +0700

    New Cinemas API

--- src/Cinemas.php
+++ src/Cinemas.php
@@ -4,24 +4,40 @@ namespace CGVSeat;
 
 use CGVSeat\Handler;
 
+/**
+ * 
+ * @package CGVSeat
+ * @author Yoppy Dimas <anggaraputra456@gmail.com|yoppy@sgbteam.id> 
+ * */
 class Cinemas extends Handler
 {
-    public function nowPlaying(string $LocationID = '023')
+    /**
+     * @param string $locationID
+     * 
+     * @return object
+     */
+    public function getLocation(string $locationID = '023')
     {
         return $this->fetch('post', 'mw/exceute', [
             "method" => "get",
             "path" => "cinemas",
             "params" => [
-                "location_id" => $LocationID,
+                "location_id" => $locationID,
             ] 
         ]);
     }
 
-    public function Show($Date, $LocationID = '023')
+    /**
+     * @param mixed $Date (20241023)
+     * @param string $locationID
+     * 
+     * @return object
+     */
+    public function show($Date, $locationID = '023')
     {
         return $this->fetch('post', 'mw/exceute', [
             "method" => "get",
-            "path" => trim("cinemas/" . $LocationID . "/schedules"),
+            "path" => trim("cinemas/" . $locationID . "/schedules"),
             "params" => [
                 "date" => $Date
             ] 
